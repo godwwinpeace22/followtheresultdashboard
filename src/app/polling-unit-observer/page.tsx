c2fb991870cd8a17b04c7a68f41eff86f3331e6b
Godwin <godwinpeace22@gmail.com>
2023-11-06 16:43:44 +0100

    initial commit

---
+++ src/app/polling-unit-observer/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import PolingUnitForm from "@/components/polling-unit-form";
+import { useState, useEffect } from "react";
+
+export default function Page() {
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    import("preline");
+  }, []);
+
+  return (
+    <div className="container max-w-4xl mx-auto px-4 py-10 lg:shadow-md">
+      <div className="flex justify-center items-center mb-20">
+        <img src="/followtheresultlogo.png" alt="" style={{ width: 200 }} />
+        {/* <span>#FollowTheResult</span> */}
+      </div>
+      <h1
+        className="text-center text-lg mb-5 font-bold"
+        style={{ fontSize: 25 }}
+      >
+        Polling Unit Observer Form
+      </h1>
+      <p className="text-center text-gray-500">
+        This form is to aid election observers and monitors send in their <br />
+        observation on the election day across all LGAs
+      </p>
+
+      {!showForm && (
+        <>
+          <div className="flex justify-center items-center mt-20">
+            <button
+              type="button"
+              className="mx-auto w-60 self-center py-3 px-4 inline-flex justify-center items-center gap-2 rounded-full border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+              onClick={() => setShowForm(true)}
+            >
+              Get Started
+            </button>
+          </div>
+
+          <div className="flex flex-wrap justify-center items-center mt-20 gap-10">
+            <img
+              src="nyff.jpeg"
+              alt="Nigeria Youth Futures Fund logo"
+              width={100}
+            />
+            <img
+              src="leap-africa-logo.jpeg"
+              alt="Leap Africa logo"
+              width={100}
+            />
+            <img
+              src="readytoleadafrica.jpeg"
+              alt="ReadyToLeadAfrica logo"
+              width={100}
+            />
+          </div>
+        </>
+      )}
+
+      {showForm && (
+        <div className="flex  justify-center items-center">
+          <PolingUnitForm />
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/components/Login.tsx
@@ -0,0 +1,197 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <div className="text-center">
+        <button
+          type="button"
+          className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+          data-hs-overlay="#hs-modal-signup"
+        >
+          Open modal
+        </button>
+      </div>
+      <div
+        id="hs-modal-signup"
+        className="hs-overlay hidden w-full h-full fixed top-0 left-0 z-[60] overflow-x-hidden overflow-y-auto"
+      >
+        <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+          <div className="bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-4 sm:p-7">
+              <div className="text-center">
+                <h2 className="block text-2xl font-bold text-gray-800 dark:text-gray-200">
+                  Sign up
+                </h2>
+                <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                  Already have an account?
+                  <a
+                    className="text-blue-600 decoration-2 hover:underline font-medium"
+                    href="../examples/html/modal-signin.html"
+                  >
+                    Sign in here
+                  </a>
+                </p>
+              </div>
+              <div className="mt-5">
+                {/* Form */}
+                <form>
+                  <div className="grid gap-y-4">
+                    {/* Form Group */}
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block text-sm mb-2 dark:text-white"
+                      >
+                        Email address
+                      </label>
+                      <div className="relative">
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                          required
+                          aria-describedby="email-error"
+                        />
+                        <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                          <svg
+                            className="h-5 w-5 text-red-500"
+                            width={16}
+                            height={16}
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                            aria-hidden="true"
+                          >
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                          </svg>
+                        </div>
+                      </div>
+                      <p
+                        className="hidden text-xs text-red-600 mt-2"
+                        id="email-error"
+                      >
+                        Please include a valid email address so we can get back
+                        to you
+                      </p>
+                    </div>
+                    {/* End Form Group */}
+                    {/* Form Group */}
+                    <div>
+                      <label
+                        htmlFor="password"
+                        className="block text-sm mb-2 dark:text-white"
+                      >
+                        Password
+                      </label>
+                      <div className="relative">
+                        <input
+                          type="password"
+                          id="password"
+                          name="password"
+                          className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                          required
+                          aria-describedby="password-error"
+                        />
+                        <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                          <svg
+                            className="h-5 w-5 text-red-500"
+                            width={16}
+                            height={16}
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                            aria-hidden="true"
+                          >
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                          </svg>
+                        </div>
+                      </div>
+                      <p
+                        className="hidden text-xs text-red-600 mt-2"
+                        id="password-error"
+                      >
+                        8+ characters required
+                      </p>
+                    </div>
+                    {/* End Form Group */}
+                    {/* Form Group */}
+                    <div>
+                      <label
+                        htmlFor="confirm-password"
+                        className="block text-sm mb-2 dark:text-white"
+                      >
+                        Confirm Password
+                      </label>
+                      <div className="relative">
+                        <input
+                          type="password"
+                          id="confirm-password"
+                          name="confirm-password"
+                          className="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                          required
+                          aria-describedby="confirm-password-error"
+                        />
+                        <div className="hidden absolute inset-y-0 right-0 flex items-center pointer-events-none pr-3">
+                          <svg
+                            className="h-5 w-5 text-red-500"
+                            width={16}
+                            height={16}
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                            aria-hidden="true"
+                          >
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                          </svg>
+                        </div>
+                      </div>
+                      <p
+                        className="hidden text-xs text-red-600 mt-2"
+                        id="confirm-password-error"
+                      >
+                        Password does not match the password
+                      </p>
+                    </div>
+                    {/* End Form Group */}
+                    {/* Checkbox */}
+                    <div className="flex items-center">
+                      <div className="flex">
+                        <input
+                          id="remember-me"
+                          name="remember-me"
+                          type="checkbox"
+                          className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                        />
+                      </div>
+                      <div className="ml-3">
+                        <label
+                          htmlFor="remember-me"
+                          className="text-sm dark:text-white"
+                        >
+                          I accept the{" "}
+                          <a
+                            className="text-blue-600 decoration-2 hover:underline font-medium"
+                            href="#"
+                          >
+                            Terms and Conditions
+                          </a>
+                        </label>
+                      </div>
+                    </div>
+                    {/* End Checkbox */}
+                    <button
+                      type="submit"
+                      className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+                    >
+                      Continue
+                    </button>
+                  </div>
+                </form>
+                {/* End Form */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
