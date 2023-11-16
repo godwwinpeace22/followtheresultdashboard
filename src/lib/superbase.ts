8598c2adfd2fcec28c0aa15c69bbee7160be3d20
Godwin <godwinpeace22@gmail.com>
2023-11-16 17:17:31 +0100

    update supabase

--- src/lib/superbase.ts
+++ src/lib/superbase.ts
@@ -14,8 +14,8 @@ const options = {
   },
 };
 const supabase = createClient<Database>(
-  "https://wlbajfmskpybsmaprewm.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsYmFqZm1za3B5YnNtYXByZXdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk1Mzc5NzAsImV4cCI6MjAxNTExMzk3MH0.bjL3t5H92VjnbpB1vd8K4xwWg-NBVHKBNELpFPagWv4",
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_TOKEN!,
   options
 );
 
